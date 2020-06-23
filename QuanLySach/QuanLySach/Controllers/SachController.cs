d767b18c5ec93254db0274773b0926600cd8ab89
Linhnguyen2802 <nguyenvulinh2802@gmail.com>
2020-06-23 09:28:45 +0700

    Lap7

---
+++ QuanLySach/QuanLySach/Controllers/SachController.cs
@@ -0,0 +1,32 @@
+﻿using QuanLySach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QuanLySach.Controllers
+{
+    public class SachController : ApiController
+    {
+        Sach[] sachs = new Sach[]
+        {
+            new Sach {Id = 1, Title = "Sách 1", AuthorName = "TG 1", Price = 10000, Content = "aaaaaaaaaaaaaaaaaaaaaaaaa"  },
+            new Sach {Id = 2, Title = "Sách 2", AuthorName = "TG 2", Price = 20000, Content = "bbbbbbbbbbbbbbbbbbbbbbbbb"},
+        }; 
+        public IEnumerable<Sach> GetAll()
+        {
+            return sachs;
+        }
+        public IHttpActionResult GetSach(int id)
+        {
+            var sach = sachs.FirstOrDefault((p) => p.Id == id );
+            if(sach == null)
+            {
+                return NotFound();
+            }
+            return Ok(sach);
+        }
+}
+}
